60ed8f81b8ed205e4f1b7b5072820c75aea76e93
hgb926 <hgb926@naver.com>
2024-11-22 21:32:56 +0900

    [Feat] 회원가입 메서드 개발 UserController

--- src/main/java/com/project/api/controller/UserController.java
+++ src/main/java/com/project/api/controller/UserController.java
@@ -1,13 +1,14 @@
 package com.project.api.controller;
 
+import com.project.api.dto.request.UserSaveDto;
 import com.project.api.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/auth")
 @Slf4j
 @RequiredArgsConstructor
 public class UserController {
@@ -20,5 +21,26 @@ public class UserController {
     public ResponseEntity<?> checkEmail(String email) {
         boolean flag = userService.checkEmailDuplication(email);
         return ResponseEntity.ok().body(flag);
+    }
+
+    // 인증코드 검증 api
+    @GetMapping("/code")
+    public ResponseEntity<?> verifyCode(String email, String code) {
+        log.info("{}'s verify code is [  {}  ]", email, code);
+        boolean isMatch = userService.isMatchCode(email, code);
+        return ResponseEntity.ok().body(isMatch);
+    }
+
+    // 회원가입 마무리 단계
+    @PostMapping("/join")
+    public ResponseEntity<?> join(@RequestBody UserSaveDto dto) {
+
+        try {
+            // DB 저장 단계
+            userService.confirmSignUp(dto);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        return ResponseEntity.ok().body("saved success ^^");
     }
 }
