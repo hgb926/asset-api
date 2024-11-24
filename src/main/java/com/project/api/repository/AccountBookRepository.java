c0907d4e3f8b5339c1ff8575f6a3ac957eae2b41
hgb926 <hgb926@naver.com>
2024-11-24 21:46:30 +0900

    [Init] AccountBook 관련 MVC 패턴 객체들 생성
    Controller, Service, Repository

---
+++ src/main/java/com/project/api/repository/AccountBookRepository.java
@@ -0,0 +1,8 @@
+package com.project.api.repository;
+
+import com.project.api.entity.AccountBook;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountBookRepository extends JpaRepository<AccountBook, Long> {
+
+}
