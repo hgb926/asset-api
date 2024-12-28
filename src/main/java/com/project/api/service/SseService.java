e4bb148bda863baa105ca63514784f2256aa69a6
hgb926 <hgb926@naver.com>
2024-12-28 20:41:27 +0900

    [Feat] Sse 실시간 알림 서비스 활성화

---
+++ src/main/java/com/project/api/service/SseService.java
@@ -0,0 +1,54 @@
+package com.project.api.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+@Slf4j
+public class SseService {
+    private final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>();
+
+    /**
+     * 사용자별 SSE 연결 생성
+     */
+    public SseEmitter connect(Long userId) {
+        SseEmitter emitter = new SseEmitter(0L); // 무제한 대기
+        emitters.put(userId, emitter);
+
+        emitter.onCompletion(() -> emitters.remove(userId));
+        emitter.onTimeout(() -> emitters.remove(userId));
+        emitter.onError((e) -> emitters.remove(userId));
+
+        return emitter;
+    }
+
+    /**
+     * 특정 사용자에게 알림 전송
+     */
+    public void sendNotification(Long userId, String message, String type, Long boardId, Long goalId) {
+        SseEmitter emitter = emitters.get(userId);
+        log.info("userId {}, message {}, type {}, boardId {}, goalId {} ", userId, message, type, boardId, goalId);
+        if (emitter != null) {
+            try {
+                Map<String, Object> data = Map.of(
+                        "message", message,
+                        "type", type,
+                        "userId", userId,
+                        "boardId", boardId,
+                        "goalId", goalId
+                );
+
+                emitter.send(SseEmitter.event()
+                        .name("notice")
+                        .data(data));
+            } catch (IOException e) {
+                emitters.remove(userId);
+            }
+        }
+    }
+}
